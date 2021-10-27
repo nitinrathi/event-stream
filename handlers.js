08c2b5c6bb03a65920a3cd0c25ed4d38b3ff7790
Nitin Rathi <rathi.k.nitin@gmail.com>
2021-10-28 00:36:03 +0530

    send fake events to server

--- handlers.js
+++ handlers.js
@@ -1,4 +1,4 @@
 module.exports = (app) => {
   app.get("/", (req, res) => res.send("ok"))
-  app.post("/events", (req, res) => res.send(req.body))
+  app.post("/events", (req, res) => console.log(req.body) || res.send(req.body))
 };
---
+++ load.js
@@ -0,0 +1,28 @@
+const faker = require("faker");
+const axios = require("axios");
+
+const username = faker.name.firstName;
+const location = faker.address.city;
+const text = faker.lorem.text;
+const tags = faker.random.arrayElements;
+const createdOn = faker.date.recent;
+
+const event = () => ({
+  "user-name": username(),
+  "location": location(),
+  "text": text(),
+  "tags": tags(),
+  "created-on": createdOn()
+});
+
+
+const request = (event) => {
+  axios({
+    method: "post",
+    url: "http://localhost:3333/events",
+    data: event
+  })
+}
+
+if (require.main === module) setInterval(() => request(event()), 100)
+
